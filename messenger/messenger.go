2a896188950f5d027d770712d7e0b66876e6a16c
Krzysztof Romanowski <castus.pl@gmail.com>
2024-12-02 21:21:24 +0100

    Change email vendor (#125)

--- messenger/messenger.go
+++ messenger/messenger.go
@@ -1,40 +1,57 @@
 package messenger
 
 import (
+	"fmt"
 	"log/slog"
+	"net/smtp"
 	"os"
 	"strings"
-
-	"github.com/sendgrid/sendgrid-go"
-	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 var log = slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-func Send(message string) {
-	m := mail.NewV3Mail()
-	from := mail.NewEmail(os.Getenv("MAIL_FROM_NAME"), os.Getenv("MAIL_FROM_EMAIL"))
-	m.SetFrom(from)
+type Mail struct {
+	Sender  string
+	Subject string
+	Body    string
+}
 
-	m.Subject = "Kalendarz imprez się zmienił"
+func buildMessage(mail Mail, to string) string {
+	msg := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\r\n"
+	msg += fmt.Sprintf("From: Zawody Speedcuberskie w Polsce <%s>\r\n", mail.Sender)
+	msg += fmt.Sprintf("To: %s\r\n", to)
+	msg += fmt.Sprintf("Subject: %s\r\n", mail.Subject)
+	msg += fmt.Sprintf("\r\n%s\r\n", mail.Body)
 
-	c := mail.NewContent("text/html", message)
-	m.AddContent(c)
+	return msg
+}
 
-	p := mail.NewPersonalization()
-	var tos = []*mail.Email{}
-	splitEmails := strings.Split(os.Getenv("NOTIFY_EMAILS"), ",")
-	for _, item := range splitEmails {
-		tos = append(tos, mail.NewEmail(item, item))
+func Send(message string) {	
+	sender := "kalendarz@krzysztofromanowski.pl"
+	user := os.Getenv("MAIL_SMTP_USER")
+	password := os.Getenv("MAIL_SMTP_PASSWORD")
+	subject := "Zmiany w zawodach Speedcuberskich"
+	body := message
+	addr := "smtp-relay.brevo.com:587"
+	host := "smtp-relay.brevo.com"
+
+	request := Mail{
+		Sender:  sender,
+		Subject: subject,
+		Body:    body,
 	}
-	p.AddTos(tos...)
-	m.AddPersonalizations(p)
 
-	client := sendgrid.NewSendClient(os.Getenv("MAIL_TOKEN"))
-	_, err := client.Send(m)
-	if err != nil {
-		log.Error("Couldn't send email", "error", err)
-	} else {
-		log.Info("Email send")
+	tos := strings.Split(os.Getenv("NOTIFY_EMAILS"), ",")
+
+	for _, email := range tos {
+		msg := buildMessage(request, email)
+		auth := smtp.PlainAuth("", user, password, host)
+		err := smtp.SendMail(addr, auth, sender, []string{email}, []byte(msg))
+
+		if err != nil {
+			log.Error("Error sending email", "recipient", email, "trace", err)
+		} else {
+			log.Info("Email sent successfully", "recipient", email)
+		}
 	}
 }
